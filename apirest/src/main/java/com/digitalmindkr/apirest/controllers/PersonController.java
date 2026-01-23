dbf298d3a509173abbfbed6d331227b419d73601
Marcos Vinicius <157753545+MVLRGTR@users.noreply.github.com>
2026-01-23 17:08:30 -0300

    Example Swagger PersonController

--- apirest/src/main/java/com/digitalmindkr/apirest/controllers/PersonController.java
+++ apirest/src/main/java/com/digitalmindkr/apirest/controllers/PersonController.java
@@ -18,9 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.digitalmindkr.apirest.data.dto.v1.PersonDTO;
 import com.digitalmindkr.apirest.services.PersonService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @RestController
 @RequestMapping(value = "/person")
+@Tag(name = "People" , description = "Endpoints for managing People") //swagger
 public class PersonController {
 
 	@Autowired // faz a injeçao de dependencia de forma transparente ao programador 
@@ -60,6 +68,26 @@ public class PersonController {
 			MediaType.APPLICATION_JSON_VALUE,
 			MediaType.APPLICATION_XML_VALUE,
 			MediaType.APPLICATION_YAML_VALUE})
+	@Operation(summary = "Find All People",
+	description = "Finds All People",
+	tags = {"People"},
+	responses = {
+			@ApiResponse(
+					description = "Success" , 
+					responseCode  = "200" , 
+					content = {
+							@Content(
+									mediaType = MediaType.APPLICATION_JSON_VALUE,
+									array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class))
+									)
+					}),
+			@ApiResponse(description = "No Content" , responseCode  = "204" , content = @Content),
+			@ApiResponse(description = "Bad Request" , responseCode  = "400" , content = @Content),
+			@ApiResponse(description = "Unauthorized" , responseCode  = "401" , content = @Content),
+			@ApiResponse(description = "Not Found" , responseCode  = "404" , content = @Content),
+			@ApiResponse(description = "Internal Serve Error" , responseCode  = "500" , content = @Content)
+	}
+	)
 	public List<PersonDTO> findAll() {
 		return service.findAll();
 	}
