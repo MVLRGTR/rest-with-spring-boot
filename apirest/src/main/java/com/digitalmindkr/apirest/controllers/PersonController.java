1a71d7e91b17ee49a2d27078b29e96c7ad576d71
Marcos Vinicius <157753545+MVLRGTR@users.noreply.github.com>
2026-01-23 17:29:07 -0300

    PersonControllerDocs
    Para que o controller faça somente sua função criei uma classe chamada PersonControllerDocs como interface e PersonController ser uma implementação

--- apirest/src/main/java/com/digitalmindkr/apirest/controllers/PersonController.java
+++ apirest/src/main/java/com/digitalmindkr/apirest/controllers/PersonController.java
@@ -15,25 +15,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.digitalmindkr.apirest.controllers.docs.PersonControllerDocs;
 import com.digitalmindkr.apirest.data.dto.v1.PersonDTO;
 import com.digitalmindkr.apirest.services.PersonService;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 @RestController
 @RequestMapping(value = "/person")
 @Tag(name = "People" , description = "Endpoints for managing People") //swagger
-public class PersonController {
+public class PersonController implements PersonControllerDocs {
 
 	@Autowired // faz a injeçao de dependencia de forma transparente ao programador 
 	private PersonService service;
 	
+	@Override
 	@PostMapping(produces = {
 			MediaType.APPLICATION_JSON_VALUE,
 			MediaType.APPLICATION_XML_VALUE ,
@@ -46,6 +43,7 @@ public class PersonController {
 		return service.create(person);
 	}
 	
+	@Override
 	@PutMapping(produces = {
 			MediaType.APPLICATION_JSON_VALUE,
 			MediaType.APPLICATION_XML_VALUE ,
@@ -58,40 +56,23 @@ public class PersonController {
 		return service.update(person);
 	}
 	
+	@Override
 	@DeleteMapping(value = "/{id}" )
 	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
+	@Override
 	@GetMapping(produces = {
 			MediaType.APPLICATION_JSON_VALUE,
 			MediaType.APPLICATION_XML_VALUE,
 			MediaType.APPLICATION_YAML_VALUE})
-	@Operation(summary = "Find All People",
-	description = "Finds All People",
-	tags = {"People"},
-	responses = {
-			@ApiResponse(
-					description = "Success" , 
-					responseCode  = "200" , 
-					content = {
-							@Content(
-									mediaType = MediaType.APPLICATION_JSON_VALUE,
-									array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class))
-									)
-					}),
-			@ApiResponse(description = "No Content" , responseCode  = "204" , content = @Content),
-			@ApiResponse(description = "Bad Request" , responseCode  = "400" , content = @Content),
-			@ApiResponse(description = "Unauthorized" , responseCode  = "401" , content = @Content),
-			@ApiResponse(description = "Not Found" , responseCode  = "404" , content = @Content),
-			@ApiResponse(description = "Internal Serve Error" , responseCode  = "500" , content = @Content)
-	}
-	)
 	public List<PersonDTO> findAll() {
 		return service.findAll();
 	}
 	
+	@Override
 	@GetMapping(value = "/{id}" ,
 	    produces = {
 	    	MediaType.APPLICATION_JSON_VALUE,
