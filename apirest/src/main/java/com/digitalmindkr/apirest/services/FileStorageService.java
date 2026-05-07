43f38481224bd34fbd6c549ea21271ea1596ce9a
Marcos Vinicius <157753545+MVLRGTR@users.noreply.github.com>
2026-05-07 14:04:44 -0300

    Update FileStorageService.java

--- apirest/src/main/java/com/digitalmindkr/apirest/services/FileStorageService.java
+++ apirest/src/main/java/com/digitalmindkr/apirest/services/FileStorageService.java
@@ -1,5 +1,49 @@
 package com.digitalmindkr.apirest.services;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.digitalmindkr.apirest.config.FileStorageConfig;
+import com.digitalmindkr.apirest.exception.FileStorageException;
+
 public class FileStorageService {
+	
+	private final Path fileStorageLocation;
+	
+    @Autowired
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        Path path = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath()
+                .toAbsolutePath().normalize();
+
+        this.fileStorageLocation = path;
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception e) {
+            throw new FileStorageException("Could not create the directory where files will be stored!", e);
+        }
+    }
+    
+    public String storeFile(MultipartFile file) {
+
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry! Filename Contains a Invalid path Sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (Exception e) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try Again!", e);
+        }
+    }
 
 }
